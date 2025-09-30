29494883405a554a1360fe3f732441b17e0fb664
fernando henrique <fernando88to@gmail.com>
2025-09-30 10:49:15 -0300

    frontend: criando o loading na pagina estado.

--- frontend/src/app/(DashboardLayout)/estado/page.tsx
+++ frontend/src/app/(DashboardLayout)/estado/page.tsx
@@ -1,12 +1,8 @@
-
-import {DataGrid} from '@mui/x-data-grid';
 import PageContainer from '@/app/(DashboardLayout)/components/container/PageContainer';
 import DashboardCard from '@/app/(DashboardLayout)/components/shared/DashboardCard';
 import {PageClientComarca} from "@/app/(DashboardLayout)/estado/client";
 import {clientBackendEstado} from "@/client/clientBackEnd";
 import {Metadata} from "next";
-
-
 
 export const dynamic = 'force-dynamic' // defaults to auto
 
