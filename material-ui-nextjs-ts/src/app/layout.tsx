bbb12fb6f0da33ae9497c128178c9a9c8711ad21
fernando henrique <fernando88to@gmail.com>
2025-10-03 08:53:30 -0300

    comit do novo front end

--- material-ui-nextjs-ts/src/app/layout.tsx
+++ material-ui-nextjs-ts/src/app/layout.tsx
@@ -1,9 +1,10 @@
+'use client';
 import * as React from 'react';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import InitColorSchemeScript from '@mui/material/InitColorSchemeScript';
-import theme from '@/theme';
+import {baselightTheme} from '@/utils/theme/DefaultColors';
 import ModeSwitch from '@/components/ModeSwitch';
 
 export default function RootLayout(props: { children: React.ReactNode }) {
@@ -12,7 +13,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
       <body>
         <InitColorSchemeScript attribute="class" />
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <ThemeProvider theme={theme}>
+          <ThemeProvider theme={baselightTheme}>
             {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
             <CssBaseline />
             <ModeSwitch />
