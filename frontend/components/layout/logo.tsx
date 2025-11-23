a314831b029d1f46c6a535174831bc7dc6811417
fernando henrique <fernando88to@gmail.com>
2025-11-23 09:08:37 -0300

    alteração do titulo do sistema

--- frontend/components/layout/logo.tsx
+++ frontend/components/layout/logo.tsx
@@ -10,7 +10,7 @@ export default function Logo({ className }: LogoProps) {
   return (
     <Link href="/" className={cn("flex items-center gap-2 px-5 py-4 font-bold", className)}>
       <img src="/logo.png" className="block h-5 w-5" alt="shadcn ui kit logo" />
-      Shadcn UI Kit <Badge variant="outline">Free</Badge>
+      SINCOR
     </Link>
   );
 }
